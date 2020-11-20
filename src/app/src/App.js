b8340471e84e2607c229bb6839ed6f7378806b67
Alden Davidson <adavidson+github@protonmail.ch>
2020-11-20 09:10:33 -0800

    added UnitFields for 'from' and 'to'

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import UnitField from './UnitField';
 
 class App extends React.Component {
   constructor(props) {
@@ -8,16 +9,20 @@ class App extends React.Component {
       fromAmt: 0,
       toAmt: 0,
       unitType: 1,
+      fromUnits: [],
+      toUnits: []
     }
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Unit Converter
-        </header>
-        <input type='text' defaultValue={this.state.fromAmt}/>
+      <div className="App-header">
+        <h1>Unit Converter</h1>
+        <div className='UnitConverter'>
+          <UnitField fromAmt={this.state.fromAmt}/>
+          &#x021c4;
+          <UnitField fromAmt={this.state.toAmt}/>
+        </div>
       </div>
     );
   }
